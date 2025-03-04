99a2bee4235cd82a409ea7df1ed38b6ca688e4cb
Krish <Krish@Krishs-MacBook-Pro.local>
2025-03-04 16:47:53 -0500

    Fixed toString for EndStation.

--- EndStation.java
+++ EndStation.java
@@ -1,17 +1,29 @@
 public class EndStation extends Station {
-
     public EndStation(String n, String c) {
         super(n, c);
     }
 
     public void makeEnd(){
-        if(getNext()!= null){
-            setNext(this);
+        if(this.next!=null){
+            setPrev(next);
+            setNext(prev);
         }
+        
     }
 
-    public String toString(){
-        return "END" + super.toString();
+    public String toString() {
+        String prevStation = "none";
+        String nextStation = "none";
+        
+        if (prev != null) {
+            prevStation = prev.name; 
+        }
+        
+        if (next != null) {
+            nextStation = next.name; 
+        }
+        return "ENDSTATION " + name + ": " + color + " line, " + "in service: " + inService + ", " + "previous station: " + prevStation + ", " + "next station: " + nextStation;
     }
+    
     
 }
